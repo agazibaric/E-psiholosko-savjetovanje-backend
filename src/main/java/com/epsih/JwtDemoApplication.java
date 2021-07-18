df24bee81075378779afc8849da96254b0f05944
agazibaric <ante.gazibaric@gmail.com>
2021-07-18 16:17:17 +0200

    Swagger API docs

--- src/main/java/com/epsih/JwtDemoApplication.java
+++ src/main/java/com/epsih/JwtDemoApplication.java
@@ -1,9 +1,12 @@
 package com.epsih;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "E-Psychological Counseling API"))
 public class JwtDemoApplication {
 
    public static void main(String[] args) {
